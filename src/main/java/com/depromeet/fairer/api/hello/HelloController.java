58df4d735e8dd729706d8de499222ec7acb37315
daeunkwak <misodaeun3@naver.com>
2022-04-05 18:10:27 +0900

    feat: 로그 설정

--- src/main/java/com/depromeet/fairer/api/hello/HelloController.java
+++ src/main/java/com/depromeet/fairer/api/hello/HelloController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class HelloController {
@@ -15,6 +17,7 @@ public class HelloController {
     @Operation(summary = "test hello", description = "hello api example")
     @GetMapping("/hello")
     public ResponseEntity<String> hello(@Parameter(description = "이름", required = true, example = "park") @RequestParam String name) {
+        log.info("log log log!!!");
         return ResponseEntity.ok("hello" + name);
     }
 }
