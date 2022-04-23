9dddb26c7e6ef2bda3285fa84f214e08f30475e9
SeungYoon Kim <dskym@naver.com>
2022-04-23 19:58:31 +0900

    배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,15 @@
 REPOSITORY=/home/ec2-user
 cd $REPOSITORY
 
-APP_NAME=fairer-dev
-JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
+JAR_NAME=fairer-dev.jar
 JAR_PATH=$REPOSITORY/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z $CURRENT_PID ]
+if ! [ -z $CURRENT_PID ]
 then
-  echo "> Nothing to end."
-else
-  echo "> kill -9 $CURRENT_PID"
   kill $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH deploy"
-nohup java -jar -Dspring.profiles.active=dev $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=dev $JAR_PATH &
