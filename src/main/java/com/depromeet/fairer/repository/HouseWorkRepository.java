0ff3622f40e1b93c5d5ac26d34bf5f4f56f7b3fb
daseulll <nldaseul@gmail.com>
2022-04-24 18:59:59 +0900

    feat: HouseWorkService 생성 및 createHouseWorks 메서드 구현

---
+++ src/main/java/com/depromeet/fairer/repository/HouseWorkRepository.java
@@ -0,0 +1,22 @@
+package com.depromeet.fairer.repository;
+
+import com.depromeet.fairer.domain.housework.Housework;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class HouseWorkRepository {
+    private final EntityManager em;
+
+    @Transactional
+    public List<Housework> createBatch(List<Housework> houseWorks) {
+        houseWorks.stream().forEach(houseWork -> em.persist(houseWork));
+
+        return houseWorks;
+    }
+}
