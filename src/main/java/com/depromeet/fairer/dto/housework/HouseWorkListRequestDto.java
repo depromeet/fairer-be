231eb251d02259757a9c3ef10c22712b6e9c0735
daseulll <nldaseul@gmail.com>
2022-04-26 12:55:58 +0900

    refactor: HouseWorkRequestDto에 @NotNull valudation 체크 추가
    - @NotNull valudation 체크 추가
    - RequestDto에서 받지 않아도 되는 값 삭제

--- src/main/java/com/depromeet/fairer/dto/housework/HouseWorkListRequestDto.java
+++ src/main/java/com/depromeet/fairer/dto/housework/HouseWorkListRequestDto.java
@@ -2,9 +2,11 @@ package com.depromeet.fairer.dto.housework;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class HouseWorkListRequestDto {
-    public List<HouseWorkRequestDto> houseWorks;
+    @NotNull
+    private List<HouseWorkRequestDto> houseWorks;
 }
