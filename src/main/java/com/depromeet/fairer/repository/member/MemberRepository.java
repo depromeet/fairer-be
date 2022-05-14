ba014fbf93350ab3700f678e7defe8801c5205de
daeunkwak <misodaeun3@naver.com>
2022-05-14 16:35:57 +0900

    fix: 집안일 전체 조회 API jwt memberId 추가 - jpa 수정

--- src/main/java/com/depromeet/fairer/repository/member/MemberRepository.java
+++ src/main/java/com/depromeet/fairer/repository/member/MemberRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
     Optional<Member> findByEmail(String email);
 
-    Member findAllById(Long memberId);
+    Member findAllByMemberId(Long memberId);
 }
