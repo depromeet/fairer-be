1c5fab1887c33b558caa1ba75820319e29015fd1
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-05-14 15:54:43 +0900

    fix: memberdto에 profile path 삭제

--- src/main/java/com/depromeet/fairer/dto/member/MemberDto.java
+++ src/main/java/com/depromeet/fairer/dto/member/MemberDto.java
@@ -10,13 +10,11 @@ import lombok.*;
 public class MemberDto {
     private Long memberId;
     private String memberName;
-    private String profilePath;
 
     public static MemberDto from(Member member) {
         return MemberDto.builder()
                 .memberId(member.getMemberId())
                 .memberName(member.getMemberName())
-                .profilePath(member.getProfilePath())
                 .build();
     }
 }
