69875d2a37d22a334e4f4414c7a9ee9d084d51e4
daseulll <nldaseul@gmail.com>
2022-06-18 15:45:26 +0900

    Revert "refactor: TeamMemberListResponseDto 제거하고 종속성 제거"
    This reverts commit 77dda38a85ed7aaeca29e5777a94cc2596258363.

--- src/main/java/com/depromeet/fairer/api/team/TeamController.java
+++ src/main/java/com/depromeet/fairer/api/team/TeamController.java
@@ -15,11 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -76,13 +72,9 @@ public class TeamController {
 
     @ApiOperation(value = "팀 멤버 정보 조회", notes = "팀에 소속된 멤버 목록 조회")
     @GetMapping("/members")
-    public ResponseEntity<Map<String, Object>> viewTeamMembers(@RequestMemberId Long memberId) {
-        Map<String, Object> result = new HashMap<>();
+    public ResponseEntity<TeamMemberListResponseDto> viewTeamMembers(@RequestMemberId Long memberId) {
         Set<Member> teamMembers = teamService.getTeamMembers(memberId);
-        List<TeamMemberResponseDto> membersDto = teamMembers.stream().map(TeamMemberResponseDto::from).collect(Collectors.toList());
-        result.put("members", membersDto);
-
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(TeamMemberListResponseDto.from(teamMembers));
     }
 
 }
