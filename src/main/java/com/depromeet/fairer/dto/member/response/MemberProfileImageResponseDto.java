fb04cdbb3b175ba0d9d21cbebf9fd4254664362e
SeungYoon Kim <dskym@naver.com>
2022-06-20 01:28:02 +0900

    feat:get profile list

---
+++ src/main/java/com/depromeet/fairer/dto/member/response/MemberProfileImageResponseDto.java
@@ -0,0 +1,13 @@
+package com.depromeet.fairer.dto.member.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class MemberProfileImageResponseDto {
+    List<String> smallImageList;
+    List<String> bigImageList;
+}
