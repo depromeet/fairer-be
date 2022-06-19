5f6f9d6604edd7b92e5ca12f4b84c4b617ecb8df
daeunkwak <misodaeun3@naver.com>
2022-06-19 18:15:53 +0900

    feat: @ApiOperation 추가

--- src/main/java/com/depromeet/fairer/api/housework/HouseWorkController.java
+++ src/main/java/com/depromeet/fairer/api/housework/HouseWorkController.java
@@ -10,6 +10,7 @@ import com.depromeet.fairer.dto.housework.request.HouseWorkRequestDto;
 import com.depromeet.fairer.dto.housework.request.HouseWorkStatusRequestDto;
 import com.depromeet.fairer.dto.housework.response.*;
 import com.depromeet.fairer.service.member.MemberService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -48,6 +49,7 @@ public class HouseWorkController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "날짜별 집안일 조회", notes = "본인 포함 팀원들의 집안일까지 모두 조회")
     @GetMapping(value = "")
     public ResponseEntity<List<HouseWorkDateResponseDto>> getHouseWork(@RequestParam("scheduledDate") String scheduledDate,
                                                                    @RequestMemberId Long memberId){
@@ -75,11 +77,13 @@ public class HouseWorkController {
         return ResponseEntity.ok(houseWorkDateResponseDtos);
     }
 
+    @ApiOperation(value = "개별 집안일 조회", notes = "")
     @GetMapping(value = "{houseWorkId}/detail")
     public ResponseEntity<HouseWorkResponseDto> getHouseWorkDetail(@PathVariable("houseWorkId") Long houseWorkId){
         return ResponseEntity.ok(houseWorkService.getHouseWorkDetail(houseWorkId));
     }
 
+    @ApiOperation(value = "집안일 완료여부 수정", notes = "toBeStatus=0이면 완료->미완료, toBeStatus=1이면 미완료->완료")
     @PatchMapping(value = "{houseWorkId}")
     public ResponseEntity<HouseWorkStatusResponseDto> updateHouseWorkStatus(@PathVariable("houseWorkId") Long houseWorkId, @RequestBody @Valid HouseWorkStatusRequestDto req){
         return ResponseEntity.ok(houseWorkService.updateHouseWorkStatus(houseWorkId, req.getToBeStatus()));
