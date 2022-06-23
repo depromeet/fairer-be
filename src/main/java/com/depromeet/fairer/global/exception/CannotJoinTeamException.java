a9a561896a3024dc65d99b17bdd7dab3c2c5da66
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-06-23 23:10:57 +0900

    feat: 팀에 구성원이 꽉 찼을 경우(6명) 예외 발생

--- src/main/java/com/depromeet/fairer/global/exception/CannotJoinTeamException.java
+++ src/main/java/com/depromeet/fairer/global/exception/CannotJoinTeamException.java
@@ -1,9 +1,10 @@
 package com.depromeet.fairer.global.exception;
 
-import javax.naming.CannotProceedException;
-
 public class CannotJoinTeamException extends RuntimeException {
     public CannotJoinTeamException() {
         super("이미 팀에 소속되어 있는 회원입니다.");
+    }
+    public CannotJoinTeamException(String message) {
+        super(message);
     }
 }
