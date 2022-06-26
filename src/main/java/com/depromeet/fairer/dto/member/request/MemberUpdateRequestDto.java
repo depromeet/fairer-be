4d95ef34f697808ad82c3265404bfe8b3d4217d4
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-06-26 21:48:47 +0900

    fix: dto 생성자 추가

--- src/main/java/com/depromeet/fairer/dto/member/request/MemberUpdateRequestDto.java
+++ src/main/java/com/depromeet/fairer/dto/member/request/MemberUpdateRequestDto.java
@@ -2,14 +2,17 @@ package com.depromeet.fairer.dto.member.request;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
 @ApiModel(value="멤버 정보 업데이트 요청 객체", description = "멤버 정보 업데이트 요청 객체")
+@AllArgsConstructor @NoArgsConstructor
 public class MemberUpdateRequestDto {
     @NotBlank
     @ApiModelProperty(value = "프로필 이미지 경로")
