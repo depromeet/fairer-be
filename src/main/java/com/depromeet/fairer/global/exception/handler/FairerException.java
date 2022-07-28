a3494b492c1231516266fa914ee149304e2d851e
seungyoon-kim <dskym@naver.com>
2022-07-28 20:40:47 +0900

    fix fcm request

---
+++ src/main/java/com/depromeet/fairer/global/exception/handler/FairerException.java
@@ -0,0 +1,7 @@
+package com.depromeet.fairer.global.exception.handler;
+
+public class FairerException extends RuntimeException {
+    public FairerException(Exception e) {
+        super(e);
+    }
+}
