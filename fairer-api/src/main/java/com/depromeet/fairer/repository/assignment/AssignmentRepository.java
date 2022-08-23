eb89f6b66a0e91bb737f06cf50943d7f6d26fd0c
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-08-23 18:42:03 +0900

    feat: 반복 집안일 삭제 api 구현

--- fairer-api/src/main/java/com/depromeet/fairer/repository/assignment/AssignmentRepository.java
+++ fairer-api/src/main/java/com/depromeet/fairer/repository/assignment/AssignmentRepository.java
@@ -3,7 +3,10 @@ package com.depromeet.fairer.repository.assignment;
 import com.depromeet.fairer.domain.assignment.Assignment;
 import com.depromeet.fairer.domain.housework.HouseWork;
 import com.depromeet.fairer.domain.member.Member;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +18,8 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     List<Assignment> findAllByHouseWorkAndMemberNotIn(HouseWork houseWork, List<Member> members);
 
     List<Assignment> findAllByMember(Member member);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Assignment a where a.houseWork.houseWorkId = :houseworkId")
+    void deleteAllByHouseworkId(@Param("houseworkId") Long houseWorkId);
 }
