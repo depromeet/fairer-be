8db56a5f4c8e4b460f7b0d5885c4d94be5d5f403
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-08-25 16:58:09 +0900

    refactor: RepeatRUDType.java -> UpdateDeletePolicyType.java 변경

--- fairer-api/src/main/java/com/depromeet/fairer/domain/housework/constant/UpdateDeletePolicyType.java
+++ fairer-api/src/main/java/com/depromeet/fairer/domain/housework/constant/UpdateDeletePolicyType.java
@@ -4,12 +4,11 @@ import com.depromeet.fairer.global.exception.FairerException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.lang.ref.ReferenceQueue;
 import java.util.Arrays;
 
 @Getter
 @AllArgsConstructor
-public enum RepeatRUDType {
+public enum UpdateDeletePolicyType {
     ONLY("O", "단일 일정"),
     HEREAFTER("H", "앞으로 일정"),
     ALL("A", "모든 일정")
@@ -18,7 +17,7 @@ public enum RepeatRUDType {
     private String alias;
     private String description;
 
-    public static RepeatRUDType of(String alias) {
+    public static UpdateDeletePolicyType of(String alias) {
         return Arrays.stream(values()).filter(t -> t.alias.equals(alias.toUpperCase()))
                 .findFirst().orElseThrow(() -> new FairerException("Requested RepeatRUDType is not found"));
     }
