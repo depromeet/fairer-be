74b079e8225a50e5c7f652b75db639cdfc340672
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-09-15 00:11:12 +0900

    refactor: 기존 repeatDayOfWeek 필드 repeatPattern 필드로 변경

--- fairer-api/src/main/java/com/depromeet/fairer/repository/repeatexception/RepeatExceptionRepository.java
+++ fairer-api/src/main/java/com/depromeet/fairer/repository/repeatexception/RepeatExceptionRepository.java
@@ -4,12 +4,11 @@ import com.depromeet.fairer.domain.repeatexception.RepeatException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepeatExceptionRepository extends JpaRepository<RepeatException, Long> {
+public interface RepeatExceptionRepository extends JpaRepository<RepeatException, Long>, RepeatExceptionCustomRepository{
 
-    @Modifying(clearAutomatically = true)
-    @Query("delete from RepeatException R where R.houseWork.houseWorkId = :houseWorkId and R.exceptionDate > R.houseWork.repeatEndDate")
-    void deleteAfterEndDate(Long houseWorkId);
+
 }
