d8f267a1c6de9cf6e87c109ec001a16d889be745
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-09-19 01:25:30 +0900

    fix: 집안일 삭제 시 이후에 있던 repeatException 삭제 구현

--- fairer-api/src/main/java/com/depromeet/fairer/repository/repeatexception/RepeatExceptionCustomRepositoryImpl.java
+++ fairer-api/src/main/java/com/depromeet/fairer/repository/repeatexception/RepeatExceptionCustomRepositoryImpl.java
@@ -1,7 +1,5 @@
 package com.depromeet.fairer.repository.repeatexception;
 
-import com.depromeet.fairer.domain.housework.HouseWork;
-import com.depromeet.fairer.domain.housework.QHouseWork;
 import com.depromeet.fairer.domain.repeatexception.QRepeatException;
 import com.depromeet.fairer.domain.repeatexception.RepeatException;
 import com.querydsl.jpa.JPAExpressions;
@@ -9,23 +7,41 @@ import com.querydsl.jpa.JPQLQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import static com.depromeet.fairer.domain.housework.QHouseWork.houseWork;
 import static com.depromeet.fairer.domain.repeatexception.QRepeatException.repeatException;
 
 @Repository
 @RequiredArgsConstructor
-public class RepeatExceptionCustomRepositoryImpl implements RepeatExceptionCustomRepository{
+public class RepeatExceptionCustomRepositoryImpl implements RepeatExceptionCustomRepository {
     private final JPQLQueryFactory factory;
 
     @Override
     public void deleteAfterEndDate(Long houseWorkId) {
-        factory.delete(repeatException)
-                .where(repeatException.in(
+        /*final QRepeatException e1 = new QRepeatException("e1");
+        factory.delete(e1)
+                .where(e1.eq(
                         JPAExpressions
-                                .selectFrom(repeatException)
+                                .select(repeatException)
+                                .from(repeatException)
                                 .innerJoin(repeatException.houseWork, houseWork)
-                                .where(houseWork.houseWorkId.eq(houseWorkId)
-                                        .and(repeatException.exceptionDate.after(houseWork.repeatEndDate)))
-                ));
+                                .on(repeatException.houseWork.houseWorkId.eq(houseWorkId)
+                                        .and(repeatException.exceptionDate.after(repeatException.houseWork.repeatEndDate))))
+                ).execute();*/
+        final Optional<RepeatException> foundRepeatException = factory.selectFrom(QRepeatException.repeatException)
+                .where(QRepeatException.repeatException.eq(
+                        JPAExpressions
+                                .selectFrom(QRepeatException.repeatException)
+                                .innerJoin(QRepeatException.repeatException.houseWork, houseWork)
+                                .on(QRepeatException.repeatException.houseWork.houseWorkId.eq(houseWorkId)
+                                        .and(QRepeatException.repeatException.exceptionDate.after(QRepeatException.repeatException.houseWork.repeatEndDate))))).fetch().stream().findFirst();
+
+        final QRepeatException target = new QRepeatException("target");
+        if (foundRepeatException.isPresent()) {
+            factory.delete(target)
+                    .where(target.eq(foundRepeatException.get()))
+                    .execute();
+        }
     }
 }
