65a216374a717b34f1316545b934b0c3a161065b
seungyoon-kim <dskym@naver.com>
2022-11-04 00:08:01 +0900

    fix: 집안일 생성 API 요일 주기 검증 로직 수정

--- fairer-api/src/main/java/com/depromeet/fairer/repository/housework/HouseWorkCustomRepositoryImpl.java
+++ fairer-api/src/main/java/com/depromeet/fairer/repository/housework/HouseWorkCustomRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.depromeet.fairer.repository.housework;
 
+import com.depromeet.fairer.domain.housework.QHouseWork;
 import com.depromeet.fairer.domain.housework.constant.RepeatCycle;
 import com.depromeet.fairer.domain.team.Team;
 import com.depromeet.fairer.dto.housework.response.HouseWorkQueryResponseDto;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanOperation;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -53,9 +55,9 @@ public class HouseWorkCustomRepositoryImpl implements HouseWorkCustomRepository 
                 .leftJoin(repeatException).on(repeatException.houseWork.eq(houseWork))
                 .where(member.memberId.eq(memberId).and(houseWork.scheduledDate.loe(date).and(houseWork.repeatEndDate.isNull().or(houseWork.repeatEndDate.goe(date))))
                         .and((houseWork.repeatCycle.eq(RepeatCycle.ONCE).and(houseWork.repeatPattern.eq(date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))))
-                            .or(houseWork.repeatCycle.eq(RepeatCycle.DAILY).and(repeatException.exceptionDate.isNull()))
-                            .or(houseWork.repeatCycle.eq(RepeatCycle.WEEKLY).and(houseWork.repeatPattern.contains(date.getDayOfWeek().toString())).and(repeatException.exceptionDate.isNull()))
-                            .or(houseWork.repeatCycle.eq(RepeatCycle.MONTHLY).and(houseWork.repeatPattern.castToNum(Integer.class).eq(date.getDayOfMonth())).and(repeatException.exceptionDate.isNull())))
+                            .or(houseWork.repeatCycle.eq(RepeatCycle.DAILY).and(getException(houseWork, date)))
+                            .or(houseWork.repeatCycle.eq(RepeatCycle.WEEKLY).and(houseWork.repeatPattern.contains(date.getDayOfWeek().toString())).and(getException(houseWork, date)))
+                            .or(houseWork.repeatCycle.eq(RepeatCycle.MONTHLY).and(houseWork.repeatPattern.castToNum(Integer.class).eq(date.getDayOfMonth())).and(getException(houseWork, date))))
                 ).fetch();
 
 
@@ -74,9 +76,15 @@ public class HouseWorkCustomRepositoryImpl implements HouseWorkCustomRepository 
                         .and(houseworkComplete.scheduledDate.eq(date)))
                 .where(houseWork.team.eq(team).and(houseWork.scheduledDate.loe(date).and(houseWork.repeatEndDate.isNull().or(houseWork.repeatEndDate.goe(date))))
                         .and((houseWork.repeatCycle.eq(RepeatCycle.ONCE).and(houseWork.repeatPattern.eq(date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))))
-                                .or(houseWork.repeatCycle.eq(RepeatCycle.DAILY).and(repeatException.exceptionDate.isNull()))
-                                .or(houseWork.repeatCycle.eq(RepeatCycle.WEEKLY).and(houseWork.repeatPattern.contains(date.getDayOfWeek().toString())).and(repeatException.exceptionDate.isNull()))
-                                .or(houseWork.repeatCycle.eq(RepeatCycle.MONTHLY).and(houseWork.repeatPattern.castToNum(Integer.class).eq(date.getDayOfMonth())).and(repeatException.exceptionDate.isNull())))
+                                .or(houseWork.repeatCycle.eq(RepeatCycle.DAILY).and(getException(houseWork, date)))
+                                .or(houseWork.repeatCycle.eq(RepeatCycle.WEEKLY).and(houseWork.repeatPattern.contains(date.getDayOfWeek().toString())).and(getException(houseWork, date)))
+                                .or(houseWork.repeatCycle.eq(RepeatCycle.MONTHLY).and(houseWork.repeatPattern.castToNum(Integer.class).eq(date.getDayOfMonth())).and(getException(houseWork, date))))
                 ).fetch();
+    }
+
+    public BooleanOperation getException(QHouseWork houseWork, LocalDate date){
+        return jpaQueryFactory.selectFrom(repeatException)
+                .where(repeatException.houseWork.houseWorkId.eq(houseWork.houseWorkId)
+                        .and(repeatException.exceptionDate.eq(date))).isNull();
     }
 }
