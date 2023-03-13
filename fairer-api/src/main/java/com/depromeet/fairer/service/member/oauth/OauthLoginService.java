075d0548a9d1d91947b40df399873de220f16ef6
bjih1999 <bjih1999@naver.com>
2023-03-14 00:43:21 +0900

    feat: ios 소셜 로그인
    but 연동 전

--- fairer-api/src/main/java/com/depromeet/fairer/service/member/oauth/OauthLoginService.java
+++ fairer-api/src/main/java/com/depromeet/fairer/service/member/oauth/OauthLoginService.java
@@ -1,29 +1,38 @@
 package com.depromeet.fairer.service.member.oauth;
 
 import com.depromeet.fairer.domain.alarm.Alarm;
+import com.depromeet.fairer.domain.member.Member;
+import com.depromeet.fairer.domain.member.constant.SocialType;
 import com.depromeet.fairer.domain.memberToken.MemberToken;
-import com.depromeet.fairer.dto.member.oauth.OAuthAttributes;
-import com.depromeet.fairer.dto.member.oauth.OauthLoginDto;
 import com.depromeet.fairer.dto.member.jwt.ResponseJwtTokenDto;
 import com.depromeet.fairer.dto.member.jwt.TokenDto;
+import com.depromeet.fairer.dto.member.oauth.OAuthAttributes;
+import com.depromeet.fairer.dto.member.oauth.OauthLoginDto;
+import com.depromeet.fairer.global.exception.BadRequestException;
 import com.depromeet.fairer.global.exception.MemberTokenNotFoundException;
 import com.depromeet.fairer.repository.alarm.AlarmRepository;
 import com.depromeet.fairer.repository.member.MemberRepository;
 import com.depromeet.fairer.repository.memberToken.MemberTokenRepository;
 import com.depromeet.fairer.service.member.jwt.TokenProvider;
 import com.depromeet.fairer.service.member.oauth.google.GoogleFeignService;
-import com.depromeet.fairer.domain.member.Member;
-import com.depromeet.fairer.domain.member.constant.SocialType;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.InvalidParameterException;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Optional;
 
 @Slf4j
@@ -85,13 +94,108 @@ public class OauthLoginService {
         return googleFeignService.getAccess(authorizationCode);
     }
 
+    public ResponseJwtTokenDto loginIos(String tokenString) {
+
+        Member requestMember;
+        GoogleIdToken idToken = getVerifiedIdToken(tokenString);
+
+        if (idToken == null) {
+            throw new BadRequestException("Invalid token");
+        }
+
+        GoogleIdToken.Payload payload = idToken.getPayload();
+
+        // Get profile information from payload
+        String email = payload.getEmail();
+
+        OAuthAttributes socialUserInfo = generateSocialInfoFromIdToken(idToken);
+
+        log.info("oauthAttributes: {}", socialUserInfo.toString());
+
+        final Optional<Member> foundMember = memberRepository.findWithTeamByEmail(email);
+
+        if (foundMember.isEmpty()) { // 기존 회원 아닐 때
+            Member newMember = Member.create(socialUserInfo);
+            requestMember = memberRepository.save(newMember);
+            alarmRepository.save(Alarm.create(requestMember));
+        } else {
+            requestMember = foundMember.get(); // 기존 회원일 때
+        }
+
+        // JWT 토큰 생성
+        ResponseJwtTokenDto responseJwtTokenDto = generateToken(requestMember);
+
+        return responseJwtTokenDto;
+    }
+
+    public ResponseJwtTokenDto generateToken(Member member) {
+        // JWT 토큰 생성
+        TokenDto tokenDto = tokenProvider.createTokenDto(member.getMemberId());
+        log.info("tokenDto: {}", tokenDto);
+
+        ResponseJwtTokenDto responseJwtTokenDto = modelMapper.map(tokenDto, ResponseJwtTokenDto.class);
+        final boolean isNewMember = StringUtils.isEmpty(member.getMemberName());
+        responseJwtTokenDto.setIsNewMember(isNewMember);
+        if (!isNewMember) {
+            responseJwtTokenDto.setMemberName(member.getMemberName());
+        }
+        responseJwtTokenDto.setMemberId(member.getMemberId());
+        responseJwtTokenDto.setHasTeam(member.hasTeam());
+
+        return responseJwtTokenDto;
+    }
+
+    public OAuthAttributes generateSocialInfoFromIdToken(GoogleIdToken idToken) {
+        GoogleIdToken.Payload payload = idToken.getPayload();
+
+        // Print user identifier
+        String userId = payload.getSubject();
+
+        // Get profile information from payload
+        String email = payload.getEmail();
+        String name = (String) payload.get("name");
+
+        return OAuthAttributes
+                    .builder()
+                        .email(StringUtils.isBlank(email) ? userId : email) // 이메일 동의 x 경우
+                        .name(name)
+                        .socialType(SocialType.GOOGLE)
+                    .build();
+    }
+
+    public GoogleIdToken getVerifiedIdToken(String idTokenString) {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(UrlFetchTransport.getDefaultInstance(), new GsonFactory())
+                .setAudience(Collections.singletonList("CLIENT_ID"))
+                .setIssuer("https://accounts.google.com")
+                .build();
+        try {
+            return verifier.verify(idTokenString);
+        } catch (GeneralSecurityException | IOException e) {
+            return null;
+        }
+    }
+
+    public GoogleIdToken getGoogleIdToken(String idToken) {
+        JsonFactory jsonFactory = new GsonFactory();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(UrlFetchTransport.getDefaultInstance(), jsonFactory)
+                .setAudience(Collections.singletonList("CLIENT_ID"))
+                .setIssuer("https://accounts.google.com")
+                .build();
+        try {
+            return verifier.verify(idToken);
+        } catch (Exception e) {
+            log.error("Google Token Verify Error: {}", e.getMessage());
+            throw new BadRequestException("Google Token Verify Error");
+        }
+    }
+
     public ResponseJwtTokenDto login(SocialType socialType, String accessToken) {
         final OauthLoginDto oauthLoginDto = OauthLoginDto.builder().accessToken(accessToken).socialType(socialType).build();
         return createMemberAndJwt(oauthLoginDto);
     }
 
-    public void validateLoginParams(String socialType, String accessToken) {
-        validateSocialType(socialType);
+    public void validateLoginParams(SocialType socialType, String accessToken) {
+//        validateSocialType(socialType);
         validateAccessToken(accessToken);
     }
 
@@ -106,6 +210,7 @@ public class OauthLoginService {
             throw new InvalidParameterException("Access 토큰값을 입력해주세요.");
         }
     }
+
 
     public void logout(String refreshToken, LocalDateTime now) {
         final MemberToken memberToken = memberTokenRepository.findByRefreshToken(refreshToken)
